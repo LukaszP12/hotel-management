b27526585a0610b86938c7965b0256d078386188
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-29 20:52:38 +0100

    corrected imports

--- src/main/java/pl/piwowarski/repositories/GuestRepository.java
+++ src/main/java/pl/piwowarski/repositories/GuestRepository.java
@@ -1,11 +1,14 @@
 package pl.piwowarski.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.piwowarski.model.Guest;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface GuestRepository {
+@Repository
+public interface GuestRepository extends JpaRepository<Guest,Long> {
 
     Optional<Guest> findById(Long id);
 
